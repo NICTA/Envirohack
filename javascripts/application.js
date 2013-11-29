42bac69514f88f83f62b76792ea27a015f067459
Lecky Lao <leckylao@au1.ibm.com>
2013-11-29 14:49:47 +1100

    add in working example and moved everything under atcin folder;

--- javascripts/application.js
+++
@@ -1,9 +0,0 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-var marker = L.marker([51.5, -0.09]).addTo(map);
-
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(map);
