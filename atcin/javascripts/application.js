3bb1d145b91615337d671e72aaa3af51ffcd818f
Lecky Lao <leckylao@au1.ibm.com>
2013-11-29 15:52:12 +1100

    add in reset button;

--- atcin/javascripts/application.js
+++ atcin/javascripts/application.js
@@ -1,10 +1,4 @@
 // var map = L.map('map').setView([33.8600, 151.2111], 13);
-var map = L.map('map').setView([-34.320178992999956,150.92623500800005], 9);
-
-L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
-  maxZoom: 18,
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-}).addTo(map);
 
 
 // L.marker([51.5, -0.09]).addTo(map)
@@ -27,7 +21,6 @@ L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/
 // ).addTo(map).bindPopup("I am a polygon.");
 
 
-var popup = L.popup();
 //
 // function onMapClick(e) {
 //   popup
@@ -38,6 +31,7 @@ var popup = L.popup();
 //
 // map.on('click', onMapClick);
 
+var map = null;
 $("#submit").click(function() {
   var input = $(this.form).find("input").val();
   $.getJSON("http://envirohack.research.nicta.com.au/fsdf-topology/units/" + input, function( data ) {
@@ -46,9 +40,16 @@ $("#submit").click(function() {
     $.getJSON(url, function( data ) {
       var features = data.features;
       if(features){
-        console.log(features.length);
         for(var i=0; i < features.length; i++){
           var coors = features[i].geometry.coordinates;
+          console.log(coors);
+          console.log(coors[0][0][0]);
+          map = L.map('map').setView(coors[0][0][0], 8);
+          L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+          }).addTo(map);
+          var popup = L.popup();
           L.multiPolygon(
             coors
           ).addTo(map).bindPopup(name);
@@ -58,3 +59,7 @@ $("#submit").click(function() {
   });
   return false;
 });
+
+$("#reset").click(function(){
+  map.remove();
+})
